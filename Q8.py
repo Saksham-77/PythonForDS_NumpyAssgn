ab251d15f83760cb4d5f0282e727db8172e9d1f9
Saksham <118709539+Saksham-77@users.noreply.github.com>
2023-10-17 00:11:48 +0530

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,8 +1,8 @@
 import numpy as np
 matrix = np.arange(1,26).reshape(5,5)
 print(matrix)
-print(matrix[:3])
-print(matrix[3:])
+print(matrix[2:,1:])
+print(matrix[:3,1])
 row_sums = np.sum(matrix, axis=1).reshape(5,1)
 print(row_sums)
 col_sums = np.sum(matrix, axis=0).reshape(1,5)
